e5d4789e99c1aea7fd7699c2fcb43ce84c0ce824
Alexander Grigoriev <agrigoriev@talend.com>
2017-06-07 10:55:13 +0200

    setting nginx resolvers valid timeout

--- site/role/manifests/management_proxy.pp
+++ site/role/manifests/management_proxy.pp
@@ -15,6 +15,15 @@ class role::management_proxy(
 
   $elasticsearch_url = "${elasticsearch_url_scheme}://${elasticsearch_host}"
 
+  $create_resolvers_command = 'echo resolver \
+  $(awk \'BEGIN{ORS=" "} $1=="nameserver" {print $2}\' /etc/resolv.conf) "valid=5s;" \
+  > /etc/nginx/resolvers.conf'
+  exec { 'create nginx resolvers.conf':
+    command => $create_resolvers_command,
+    creates => '/etc/nginx/resolvers.conf',
+    before  => Nginx::Resource::Vhost['es-sys'],
+  }
+
   if $auth_username and $auth_password {
     $htpasswd_file = '/etc/nginx/.htpasswd'
     file { $htpasswd_file:
@@ -43,8 +52,12 @@ class role::management_proxy(
     listen_port          => '8080',
     auth_basic           => $auth_basic,
     auth_basic_user_file => $auth_basic_user_file,
-    proxy                => $elasticsearch_url,
+    proxy                => '$proxy_url',
     location_raw_append  => ['proxy_pass_request_headers off;'],
+    location_raw_prepend => [
+      'include /etc/nginx/resolvers.conf;',
+      "set \$proxy_url ${elasticsearch_url};"
+    ],
   }
 
 }
