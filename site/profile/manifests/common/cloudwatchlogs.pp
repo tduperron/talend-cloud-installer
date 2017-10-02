88a86ed5a1e33460ef751607e543e187fbf7b7eb
Erwan SEITE <eseite@talend.com>
2017-10-02 23:00:22 +0200

    fix systemlogs where sub_stack is defined for cloudwatchlogs

--- site/profile/manifests/common/cloudwatchlogs.pp
+++ site/profile/manifests/common/cloudwatchlogs.pp
@@ -2,14 +2,15 @@
 # Configures cloudwatch with hiera-defined log files
 #
 class profile::common::cloudwatchlogs (
-
   $include = true,
-
+  $recursive = true
 ) {
   if $include {
-    create_resources(
-      '::cloudwatchlogs::log',
-      hiera_hash('cloudwatchlog_files', {})
-    )
+    if $recursive {
+      $logs_to_export = hiera_hash('cloudwatchlog_files', {})
+    } else {
+      $logs_to_export = hiera('cloudwatchlog_files', {})
+    }
+    create_resources('::cloudwatchlogs::log', $logs_to_export)
   }
 }
