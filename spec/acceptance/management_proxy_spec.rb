be9f318e77b4c798e4485da2d01b47f9fb9368ed
Alexander Grigoriev <agrigoriev@talend.com>
2017-10-03 10:42:35 +0200

    configuring elasticsearch proxies on the management_proxy

--- spec/acceptance/management_proxy_spec.rb
+++ spec/acceptance/management_proxy_spec.rb
@@ -12,12 +12,22 @@ describe 'role::management_proxy' do
     its(:stdout) { should include 'HTTP/1.1 401 Unauthorized' }
   end
 
+  describe command('/usr/bin/curl -v http://localhost:8080/sys3/') do
+    its(:stdout) { should_not include 'You Know, for Search' }
+    its(:stdout) { should include 'HTTP/1.1 401 Unauthorized' }
+  end
+
   describe command('/usr/bin/curl -v -u "kibana:mypassword" http://localhost:8080/') do
     its(:stdout) { should include 'You Know, for Search' }
     its(:stdout) { should include 'HTTP/1.1 200 OK' }
   end
 
-  describe command('/usr/bin/curl -v -u "kibana:mypassword" http://localhost:8080/sys2/') do
+  describe command('/usr/bin/curl -v -u "kibana2:mypassword" http://localhost:8080/sys2/') do
+    its(:stdout) { should include 'You Know, for Search' }
+    its(:stdout) { should include 'HTTP/1.1 200 OK' }
+  end
+
+  describe command('/usr/bin/curl -v -u "kibana3:mypassword" http://localhost:8080/sys3/') do
     its(:stdout) { should include 'You Know, for Search' }
     its(:stdout) { should include 'HTTP/1.1 200 OK' }
   end
