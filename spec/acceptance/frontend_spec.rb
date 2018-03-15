4b0a0204a2c30a06d9c1092de9df3500b0194fbb
Igor Lazebny <ilazebny@talend.com>
2018-03-15 22:16:22 +0200

    DEVOPS-3926: Remove ROOT webapp in TIC frontend tomcat config (#115)

--- spec/acceptance/frontend_spec.rb
+++ spec/acceptance/frontend_spec.rb
@@ -12,7 +12,7 @@ describe 'role::frontend' do
     it { should be_listening }
   end
 
-  describe command('/usr/bin/wget -O - http://127.0.0.1:8081') do
+  describe command('/usr/bin/wget -O - http://127.0.0.1:8081/ipaas') do
     its(:stdout) { should include '<title>Integration Cloud | Talend</title>' }
   end
 
@@ -49,11 +49,6 @@ describe 'role::frontend' do
     its(:stdout) { should include '-Xmx1024m' }
     its(:stdout) { should include '-XX:MaxMetaspaceSize=512m' }
     its(:stdout) { should include '-Djava.io.tmpdir=/srv/tomcat/ipaas-srv/temp' }
-  end
-
-  describe file('/srv/tomcat/ipaas-srv/webapps/ROOT') do
-    it { should be_symlink }
-    it { should be_linked_to '/srv/tomcat/ipaas-srv/webapps/ipaas' }
   end
 
   %w(
