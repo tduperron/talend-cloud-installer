b834e1e00b6b1d08436770caf29a7b84d18da68e
Erwan SEITE <wanix@users.noreply.github.com>
2018-04-26 18:15:08 +0200

    Devops 4219 new puppet cloudwatch (#138)
    * fix missing epel for lnav
    
    * bump talend-cloudwatch

--- site/profile/manifests/common/packages.pp
+++ site/profile/manifests/common/packages.pp
@@ -5,10 +5,12 @@ class profile::common::packages {
 
   require ::profile::common::packagecloud_repos
   require ::pip
+  ensure_packages({
+    'epel-release' => { ensure => 'present'},
+  })
 
   create_resources(
     Package,
-    hiera_hash('common_packages', {})
+    hiera_hash('common_packages', {require => Package['epel-release']})
   )
-
 }
