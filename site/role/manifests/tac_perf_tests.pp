79c49b301529c571343c471c008cfc1c38393c8c
Mykhailo Sevastian <msevastian@talend.com>
2018-07-18 02:21:10 +0300

    update

--- site/role/manifests/tac_perf_tests.pp
+++ site/role/manifests/tac_perf_tests.pp
@@ -3,9 +3,9 @@
 #
 class role::tac_perf_tests () {
   require ::profile::base
-  # require ::profile::docker::host
-  # require ::profile::docker::registry
-  # require ::profile::tac_perf_tests
+  require ::profile::docker::host
+  require ::profile::docker::registry
+  require ::profile::tac_perf_tests
   role::register_role { 'tac_perf_tests': }
 
 }
