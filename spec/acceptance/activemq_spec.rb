d11211ed030a53a16f2981b1b2394e185e514ad8
Igor Lazebny <ilazebny@talend.com>
2018-07-26 10:10:03 +0300

    DEVOPS-4905: Upgrade java to 1.8.0_181 for ActiveMQ (#166)

--- spec/acceptance/activemq_spec.rb
+++ spec/acceptance/activemq_spec.rb
@@ -4,4 +4,8 @@ describe 'role::activemq' do
   it_behaves_like 'profile::base'
   it_behaves_like 'profile::activemq'
   it_behaves_like 'role::defined', 'activemq'
+
+  describe package('jre1.8') do
+    it { should be_installed.with_version('1.8.0_181-fcs') }
+  end
 end
