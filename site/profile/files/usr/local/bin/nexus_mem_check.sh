afba0b8b923d32fcdcb811f4ea6c7bdd190e9d33
arunjadhavt <35606907+arunjadhavt@users.noreply.github.com>
2018-11-20 07:42:22 -0800

    Nexus restart flag changed to Available from Free (#222)
    * cheking available flag instead of free
    
    * replaces free falg with available
    
    * changed comment

--- site/profile/files/usr/local/bin/nexus_mem_check.sh
+++ site/profile/files/usr/local/bin/nexus_mem_check.sh
@@ -2,8 +2,8 @@
 
 # script to check memory utilization and restart nexus process
 
-free=$(free -mt | grep Total | awk '{print $4}')
-## check if free memory is less or equals to  500MB
+free=$(free -mt | grep Mem | awk '{print $7}')
+## check if available  memory is less or equals to  500MB
 if [[ "$free" -le 500  ]]; then
   INSTANCE_COUNT=1
   INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
