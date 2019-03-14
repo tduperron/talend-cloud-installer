740042c2aad221ee05ed673203a87d7ee97df21f
Erwan SEITE <wanix@users.noreply.github.com>
2019-03-14 14:07:12 +0100

    Devops-6319: zookeeper fix (#258)
    * add instance code + updt system
    
    * up monitoring solution
    
    * add time for zookeeper starting
    
    * DEVOPS-6319: fix zookeeper start
    
    * Add new log management: /var/log/zookeeper/zookeeper.log

--- spec/acceptance/shared/zookeeper.rb
+++ spec/acceptance/shared/zookeeper.rb
@@ -4,7 +4,12 @@ shared_examples 'profile::zookeeper' do
   it_behaves_like 'profile::common::packagecloud_repos'
   it_behaves_like 'profile::common::cloudwatchlog_files', %w(
     /opt/apache-tomcat/logs/catalina.out
+    /var/log/zookeeper/zookeeper.log
   )
+
+  describe command('/usr/bin/sleep 120') do
+    its(:exit_status) { should eq 0 }
+  end
 
   describe port(2181) do
     it { should be_listening }
@@ -16,6 +21,11 @@ shared_examples 'profile::zookeeper' do
 
   describe package('jre-jce') do
     it { should_not be_installed }
+  end
+
+  describe 'Exhibitor package' do
+    subject { command('/bin/rpm -q netflix-exhibitor-tomcat') }
+    its(:stdout) { should include 'netflix-exhibitor-tomcat-1.7.1-1.x86_64' }
   end
 
   describe file('/etc/rc.d/init.d/zookeeper') do
@@ -45,5 +55,4 @@ shared_examples 'profile::zookeeper' do
   describe command('echo ruok | /bin/nc 127.0.0.1 2181') do
     its(:stdout) { should eq 'imok' }
   end
-
 end
